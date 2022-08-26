fc9a9a9998a95e718a62c919b75aff1e4bcbd8e4
tania <taniaosano15@gmail.com>
2022-08-26 23:07:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+//deletes the element
+main.remove();
+
+
+const element = document.getElementById("main");
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
